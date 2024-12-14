f46c7bb4202663d293248bab1b6ee50a378acb3b
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-12-13 23:40:38 -0300

    update22

--- src/api/Server.mjs
+++ src/api/Server.mjs
@@ -1,42 +1,41 @@
-import { Sequelize, DataTypes } from 'sequelize'
+import { Sequelize, DataTypes } from "sequelize";
 
-const sequelize = new Sequelize('costs', 'root', '26340521', {
-    host: '0.0.0.0',
-    dialect: 'mysql',
-    dialectOptions: {
-        socketPath: '/var/run/mysqld/mysqld.sock'
-    }
-})
+const sequelize = new Sequelize("costs", "root", "26340521", {
+	host: "127.0.0.1",
+	dialect: "mysql",
+	port: 3306,
+	socketPath: "/var/run/mysqld/mysqld.sock",
+});
 
-sequelize.authenticate().then(console.log('autenticado'))
+sequelize.authenticate().then(console.log("autenticado"));
 
-const Project = sequelize.define('projects', {
-    id: { 
-        type: DataTypes.INTEGER, 
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-    },
+const Project = sequelize.define("projects", {
+	id: {
+		type: DataTypes.INTEGER,
+		autoIncrement: true,
+		primaryKey: true,
+		allowNull: false,
+	},
 
-    name: { 
-        type: DataTypes.STRING, 
-        allowNull: false,
-        onUpdate: true
-    },
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		onUpdate: true,
+	},
 
-    buy: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        onUpdate: true
-    },
+	buy: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		onUpdate: true,
+	},
 
-    categoria_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        onUpdate: true
-    }
-})
+	categoria_id: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		onUpdate: true,
+	},
+});
 
 //Project.sync({force: true})
 
-export default Project
+export default Project;
